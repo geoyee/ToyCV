552fa6ed58317deb15c29fad566d810074a08ad4
geoyee <geoyee@yeah.net>
2023-04-20 00:32:23 +0800

    Init and test in windows

---
+++ src/toycv.cpp
@@ -0,0 +1,12 @@
+﻿#include "toycv.h"
+
+char* tcv::readJPEG(const std::string& filePath)
+{
+	std::ifstream is(filePath, std::ifstream::in);
+	is.seekg(0, is.end);
+	int length = is.tellg();
+	is.seekg(0, is.beg);
+	char* buffer = new char[length];
+	is.read(buffer, length);
+	return buffer;
+}
---
+++ src/toycv.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include <iostream>
+#include <fstream>
+
+#define Interface __declspec(dllexport)
+
+namespace tcv {
+	// 读取JPEG
+	Interface char* readJPEG(const std::string& filePath);
+}
