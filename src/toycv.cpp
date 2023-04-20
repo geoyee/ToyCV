b2e7569327b07683fb37777e77df364359454443
geoyee <geoyee@yeah.net>
2023-04-20 12:31:30 +0800

    Test pass in windows

--- src/toycv.cpp
+++ src/toycv.cpp
@@ -6,11 +6,12 @@
 
 char* tcv::readJPEG(const std::string& filePath)
 {
-	std::ifstream is(filePath, std::ifstream::in);
+	std::ifstream is(filePath, std::ifstream::in | std::ios::binary);
 	is.seekg(0, is.end);
 	int length = is.tellg();
 	is.seekg(0, is.beg);
 	char* buffer = new char[length];
 	is.read(buffer, length);
+	is.close();
 	return buffer;
 }
