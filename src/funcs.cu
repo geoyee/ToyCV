ed763006e3ea9084cd275b3e583f338dadb4f63e
geoyee <geoyee@yeah.net>
2023-05-06 12:21:35 +0800

    Add error check

--- src/funcs.cu
+++ src/funcs.cu
@@ -1,16 +1,19 @@
 ﻿#include <stdio.h>
 #include "funcs.h"
+#include "error.cuh"
 
-// FIXME: 加法错误
 template<typename T>
-__global__ void _addNumberKernel(T** data, size_t W, size_t H, double number)
+__global__ void _addNumberKernel(T** data, size_t fLen, size_t C, double val)
 {
-	const int y = threadIdx.x;
-	const int x = blockIdx.x;
-	data[y][x] = static_cast<T>(data[y][x] + number);
+	size_t idx_x = threadIdx.x + blockIdx.x * blockDim.x;
+	size_t idx_y = threadIdx.y + blockIdx.y * blockDim.y;
+	if (idx_x < fLen && idx_y < C)
+	{
+		data[idx_y][idx_x] = static_cast<T>(data[idx_y][idx_x] + val);
+	}
 };
 
-// FIXME: 无效205
+// FIXME: 36行 [700] 遇到非法内存访问
 template<typename T>
 void _addNumberInplace(T** data, size_t fLen, size_t C, double val)
 {
@@ -21,12 +24,18 @@ void _addNumberInplace(T** data, size_t fLen, size_t C, double val)
 	T** imCuda;
 	size_t pitch;
 	size_t size = sizeof(T) * fLen;
-	cudaMallocPitch((void**)&imCuda, &pitch, size, C);
-	cudaMemset2D(imCuda, pitch, 0, size, C);
-	cudaMemcpy2D(imCuda, pitch, data, size, size, C, cudaMemcpyHostToDevice);
-	_addNumberKernel<T><<<fLen, C>>>(imCuda, fLen, C, val);
-	cudaMemcpy2D(imCpu, size, imCuda, pitch, size, C, cudaMemcpyDeviceToHost);
-	cudaFree(imCuda);
+	CHECK(cudaMallocPitch((void**)&imCuda, &pitch, size, C));
+	CHECK(cudaMemset2D(imCuda, pitch, 0, size, C));
+	CHECK(cudaMemcpy2D(imCuda, pitch, data, size, size, C, cudaMemcpyHostToDevice));
+	dim3 block_size(16, 16);
+	dim3 grid_size((fLen + block_size.x - 1) / block_size.x, (C + block_size.y - 1) / block_size.y);
+	CHECK(cudaDeviceSynchronize());
+	_addNumberKernel<T><<<grid_size, block_size>>>(imCuda, fLen, C, val);
+	// 检查核函数需要在调用核函数后手动加上这两句
+	CHECK(cudaGetLastError());
+	CHECK(cudaDeviceSynchronize());
+	CHECK(cudaMemcpy2D(imCpu, size, imCuda, pitch, size, C, cudaMemcpyDeviceToHost));
+	CHECK(cudaFree(imCuda));
 	for (int i = 0; i < 10; ++i)
 		printf("old val: %d and new val: %d\n", data[0][i], imCpu[0][i]);
 	data = imCpu;
